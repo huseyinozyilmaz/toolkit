e07a70dda1766a75fefd78b2c5f7dd8bc6385c5a
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2019-01-12 22:39:01 +0000

    Define a proxy to route all /api calls to server

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  outputDir: path.join(__dirname, 'server', 'public'),
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081'
+      }
+    }
+  }
+}
